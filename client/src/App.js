8de6167a39ba1413b421bf67b0f4e47a7aabdf70
ShobhitNair <shobhit842160@gmail.com>
2024-03-14 18:32:27 +0530

    updae

--- client/src/App.js
+++ client/src/App.js
@@ -13,7 +13,9 @@ function App() {
 
     try {
       await axios.post("https://imageuploader-server.vercel.app/upload", formdata);
-
+      const response = await axios.get("https://imageuploader-server.vercel.app/getImage");
+      setImages(response.data.map((item) => item.image));
+      console.log(response.data)
     } catch (error) {
       console.log(error);
     }
